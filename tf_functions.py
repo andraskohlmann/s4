dbbcd18ea6e73a4fad54692b669d049a2e54e40c
Kohlmann András <metuoku@outlook.com>
2019-07-20 15:57:37 +0200

    refactored tf functions

---
+++ tf_functions.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+
+
+# @tf.function
+def augment(image):
+    flipped = tf.image.flip_left_right(image)
+    images = tf.stack([image, flipped])
+    params = {
+        'flip': [0, 1]
+    }
+    return images, params
+
+
+# @tf.function
+def consistence_loss(predictions, params):
+    augmented_preds = tf.unstack(predictions[1:])
+    loss = 0.
+    for i, pred in enumerate(augmented_preds):
+        augmented = predictions[0]
+        if params['flip'][i + 1] == 1:
+            augmented = tf.image.flip_left_right(augmented)
+        loss += tf.reduce_mean(tf.losses.mean_absolute_error(augmented, pred))
+    return loss
+
+
+# @tf.function
+def train(model, batch_data, optimizer):
+    with tf.GradientTape() as tape:
+        images, labels = batch_data
+        logits = model(images)
+
+        preds = tf.argmax(tf.nn.softmax(logits), axis=-1)
+
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+        loss = tf.reduce_mean(loss)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(grads_and_vars=zip(gradients, model.trainable_variables))
+        return loss, images, labels, preds
+
+
+# @tf.function
+def validate(model, batch_data):
+    images, labels = batch_data
+    logits = model(images)
+
+    preds = tf.argmax(tf.nn.softmax(logits), axis=-1)
+
+    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+    loss = tf.reduce_mean(loss)
+    return loss, images, labels, preds
--- training.py
+++ training.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 
 from data import cityscapes
 from model import resnet50_fcn
+from tf_functions import train, validate
 from utils import allow_growth
 
 allow_growth()
@@ -18,55 +19,6 @@ flags.DEFINE_integer('debug_freq', -1, 'EDebug output freq')
 flags.DEFINE_list('resolution', ['128', '256'], 'Resolution')
 flags.DEFINE_string('input', '/Users/metuoku/data/cityscapes/', 'Cityscapes input folder')
 flags.DEFINE_string('tb_dir', 'logs', 'Tensorboard')
-
-
-# @tf.function
-def augment(image):
-    flipped = tf.image.flip_left_right(image)
-    images = tf.stack([image, flipped])
-    params = {
-        'flip': [0, 1]
-    }
-    return images, params
-
-
-# @tf.function
-def consistence_loss(predictions, params):
-    augmented_preds = tf.unstack(predictions[1:])
-    loss = 0.
-    for i, pred in enumerate(augmented_preds):
-        augmented = predictions[0]
-        if params['flip'][i + 1] == 1:
-            augmented = tf.image.flip_left_right(augmented)
-        loss += tf.reduce_mean(tf.losses.mean_absolute_error(augmented, pred))
-    return loss
-
-
-# @tf.function
-def train(model, batch_data, optimizer):
-    with tf.GradientTape() as tape:
-        images, labels = batch_data
-        logits = model(images)
-
-        preds = tf.argmax(tf.nn.softmax(logits), axis=-1)
-
-        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
-        loss = tf.reduce_mean(loss)
-        gradients = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(grads_and_vars=zip(gradients, model.trainable_variables))
-        return loss, images, labels, preds
-
-
-# @tf.function
-def validate(model, batch_data):
-    images, labels = batch_data
-    logits = model(images)
-
-    preds = tf.argmax(tf.nn.softmax(logits), axis=-1)
-
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
-    loss = tf.reduce_mean(loss)
-    return loss, images, labels, preds
 
 
 resolution = [int(_) for _ in FLAGS.resolution]
