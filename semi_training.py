2210d040fb3021f78c86616bf962a271c0b0479f
Kohlmann András <metuoku@outlook.com>
2019-10-31 13:17:16 +0100

    absl config files

--- semi_training.py
+++ semi_training.py
@@ -1,7 +1,8 @@
 import os
 
 import tensorflow as tf
-from tensorflow.python.platform import flags
+from absl import app
+from absl import flags
 
 from data import cityscapes, cityscapes_unlabeled
 from model import resnet50_fcn
@@ -15,85 +16,91 @@ FLAGS = flags.FLAGS
 flags.DEFINE_integer('batch_size', 4, 'Batch size')
 flags.DEFINE_integer('unlabeled_batch_size', 2, 'Unlabeled batch size')
 flags.DEFINE_integer('limit', 1200, 'Limit')
-flags.DEFINE_integer('epoch', 100, 'Epoch number')
+flags.DEFINE_integer('epoch', 10, 'Epoch number')
 flags.DEFINE_integer('debug_freq', -1, 'Debug output freq')
 
-
-flags.DEFINE_string('lr', '0.001', 'learning rate')
+flags.DEFINE_string('learning_rate', '0.001', 'learning rate')
 
 flags.DEFINE_list('resolution', ['128', '256'], 'Resolution')
 flags.DEFINE_string('input', '/Users/metuoku/data/cityscapes/', 'Cityscapes input folder')
-flags.DEFINE_string('run', 'baseline_semi_no_mixup_flip', 'Experiment run')
-flags.DEFINE_boolean('cont', True, 'Continue training from ckpt')
+flags.DEFINE_boolean('cont', False, 'Continue training from ckpt')
+flags.DEFINE_string('run', None, 'Experiment run')
+flags.mark_flag_as_required('run')
 
-FLAGS.resolution = [int(_) for _ in FLAGS.resolution]
-FLAGS.lr = float(FLAGS.lr)
-num_classes = 19
 
-labeled_dataset, labeled_size = cityscapes(
-    FLAGS.input,
-    state='train',
-    resize_dims=FLAGS.resolution,
-    batch_size=FLAGS.batch_size,
-    take=FLAGS.limit,
-    shuffle=True
-)
-unlabeled_dataset, unlabeled_size = cityscapes_unlabeled(
-    FLAGS.input,
-    state='train',
-    resize_dims=FLAGS.resolution,
-    batch_size=FLAGS.unlabeled_batch_size,
-    skip=FLAGS.limit,
-    shuffle=True
-)
-train_dataset = tf.data.Dataset.zip((labeled_dataset, unlabeled_dataset))
-train_size = labeled_size + unlabeled_size
-val_dataset, val_size = cityscapes(
-    FLAGS.input,
-    state='val',
-    resize_dims=FLAGS.resolution,
-    batch_size=FLAGS.batch_size
-)
-fcn = resnet50_fcn(n_classes=num_classes)
-adam = tf.optimizers.Adam(learning_rate=FLAGS.lr)
-# adam = tf.keras.optimizers.SGD(lr=FLAGS.lr)
-b = 0
+def main(argv):
+    FLAGS.resolution = [int(_) for _ in FLAGS.resolution]
+    FLAGS.learning_rate = float(FLAGS.learning_rate)
+    num_classes = 19
 
-# tb logs
-train_summary_writer = tf.summary.create_file_writer('{}/{}/train'.format(FLAGS.log_dir, FLAGS.run))
-val_summary_writer = tf.summary.create_file_writer('{}/{}/val'.format(FLAGS.log_dir, FLAGS.run))
-if not os.path.exists(os.path.join('out', FLAGS.run)):
-    os.makedirs(os.path.join('out', FLAGS.run), exist_ok=True)
+    labeled_dataset, labeled_size = cityscapes(
+        FLAGS.input,
+        state='train',
+        resize_dims=FLAGS.resolution,
+        batch_size=FLAGS.batch_size,
+        take=FLAGS.limit,
+        shuffle=True
+    )
+    unlabeled_dataset, unlabeled_size = cityscapes_unlabeled(
+        FLAGS.input,
+        state='train',
+        resize_dims=FLAGS.resolution,
+        batch_size=FLAGS.unlabeled_batch_size,
+        skip=FLAGS.limit,
+        shuffle=True
+    )
+    train_dataset = tf.data.Dataset.zip((labeled_dataset, unlabeled_dataset))
+    train_size = labeled_size + unlabeled_size
+    val_dataset, val_size = cityscapes(
+        FLAGS.input,
+        state='val',
+        resize_dims=FLAGS.resolution,
+        batch_size=FLAGS.batch_size
+    )
+    fcn = resnet50_fcn(n_classes=num_classes)
+    adam = tf.optimizers.Adam(learning_rate=FLAGS.learning_rate)
+    # adam = tf.keras.optimizers.SGD(lr=FLAGS.lr)
+    b = 0
 
-# checkpointing
-ckpt, manager, init_epoch = checkpoints(adam, fcn)
-# min_val_loss = np.inf
-max_miou = 0
+    # tb logs
+    train_summary_writer = tf.summary.create_file_writer('{}/{}/train'.format(FLAGS.log_dir, FLAGS.run))
+    val_summary_writer = tf.summary.create_file_writer('{}/{}/val'.format(FLAGS.log_dir, FLAGS.run))
+    if not os.path.exists(os.path.join('out', FLAGS.run)):
+        os.makedirs(os.path.join('out', FLAGS.run), exist_ok=True)
 
-# metrics
-avg_loss = tf.keras.metrics.Mean(name='loss', dtype=tf.float32)
-mIoU = tf.keras.metrics.MeanIoU(num_classes=num_classes, dtype=tf.float32)
+    # checkpointing
+    ckpt, manager, init_epoch = checkpoints(adam, fcn)
+    # min_val_loss = np.inf
+    max_miou = 0
 
-for i in range(init_epoch, init_epoch + FLAGS.epoch):
-    with train_summary_writer.as_default():
-        print('train epoch ', i)
-        semisupervised_train_loop(fcn, adam, train_dataset, avg_loss, mIoU, iters=train_size // FLAGS.batch_size)
-        tf.summary.scalar('loss', avg_loss.result(), step=i)
-        tf.summary.scalar('mIoU', mIoU.result(), step=i)
-        avg_loss.reset_states()
-        mIoU.reset_states()
+    # metrics
+    avg_loss = tf.keras.metrics.Mean(name='loss', dtype=tf.float32)
+    mIoU = tf.keras.metrics.MeanIoU(num_classes=num_classes, dtype=tf.float32)
 
-    with val_summary_writer.as_default():
-        print('val epoch ', i)
-        val_loop(fcn, val_dataset, avg_loss, mIoU, iters=val_size // FLAGS.batch_size)
-        ckpt.step.assign_add(1)
+    for i in range(init_epoch, init_epoch + FLAGS.epoch):
+        with train_summary_writer.as_default():
+            print('train epoch ', i)
+            semisupervised_train_loop(fcn, adam, train_dataset, avg_loss, mIoU, iters=train_size // FLAGS.batch_size)
+            tf.summary.scalar('loss', avg_loss.result(), step=i)
+            tf.summary.scalar('mIoU', mIoU.result(), step=i)
+            avg_loss.reset_states()
+            mIoU.reset_states()
 
-        tf.summary.scalar('loss', avg_loss.result(), step=i)
-        tf.summary.scalar('mIoU', mIoU.result(), step=i)
+        with val_summary_writer.as_default():
+            print('val epoch ', i)
+            val_loop(fcn, val_dataset, avg_loss, mIoU, iters=val_size // FLAGS.batch_size)
+            ckpt.step.assign_add(1)
 
-        if mIoU.result() > max_miou or True:
-            manager.save()
-            max_miou = mIoU.result()
+            tf.summary.scalar('loss', avg_loss.result(), step=i)
+            tf.summary.scalar('mIoU', mIoU.result(), step=i)
 
-        avg_loss.reset_states()
-        mIoU.reset_states()
+            if mIoU.result() > max_miou or True:
+                manager.save()
+                max_miou = mIoU.result()
+
+            avg_loss.reset_states()
+            mIoU.reset_states()
+
+
+if __name__ == '__main__':
+    app.run(main)
--- training.py
+++ training.py
@@ -1,14 +1,13 @@
 import os
 
 import tensorflow as tf
-from tensorflow.python.platform import flags
+from absl import app
+from absl import flags
 
 from data import cityscapes
 from model import resnet50_fcn
 from tf_functions import supervised_train_loop, val_loop
 from utils import allow_growth, checkpoints
-
-allow_growth()
 
 FLAGS = flags.FLAGS
 
@@ -17,70 +16,78 @@ flags.DEFINE_integer('limit', -1, 'Limit')
 flags.DEFINE_integer('epoch', 10, 'Epoch number')
 flags.DEFINE_integer('debug_freq', -1, 'Debug output freq')
 
-
-flags.DEFINE_string('lr', '0.001', 'learning rate')
+flags.DEFINE_string('learning_rate', '0.001', 'learning rate')
 
 flags.DEFINE_list('resolution', ['128', '256'], 'Resolution')
 flags.DEFINE_string('input', '/Users/metuoku/data/cityscapes/', 'Cityscapes input folder')
-flags.DEFINE_string('run', 'default', 'Experiment run')
 flags.DEFINE_boolean('cont', False, 'Continue training from ckpt')
+flags.DEFINE_string('run', None, 'Experiment run')
+flags.mark_flag_as_required('run')
 
-FLAGS.resolution = [int(_) for _ in FLAGS.resolution]
-FLAGS.lr = float(FLAGS.lr)
-num_classes = 19
 
-train_dataset, train_size = cityscapes(
-    FLAGS.input,
-    state='train',
-    resize_dims=FLAGS.resolution,
-    batch_size=FLAGS.batch_size,
-    take=FLAGS.limit
-)
-val_dataset, val_size = cityscapes(
-    FLAGS.input,
-    state='val',
-    resize_dims=FLAGS.resolution,
-    batch_size=FLAGS.batch_size
-)
-fcn = resnet50_fcn(n_classes=num_classes)
-adam = tf.keras.optimizers.Adam(lr=FLAGS.lr)
-b = 0
+def main(argv):
+    FLAGS.resolution = [int(_) for _ in FLAGS.resolution]
+    FLAGS.learning_rate = float(FLAGS.learning_rate)
+    num_classes = 19
 
-# tb logs
-train_summary_writer = tf.summary.create_file_writer('{}/{}/train'.format(FLAGS.log_dir, FLAGS.run))
-val_summary_writer = tf.summary.create_file_writer('{}/{}/val'.format(FLAGS.log_dir, FLAGS.run))
-if not os.path.exists(os.path.join('out', FLAGS.run)):
-    os.makedirs(os.path.join('out', FLAGS.run), exist_ok=True)
+    allow_growth()
 
-# checkpointing
-ckpt, manager, init_epoch = checkpoints(adam, fcn)
-# min_val_loss = np.inf
-max_miou = 0
+    train_dataset, train_size = cityscapes(
+        FLAGS.input,
+        state='train',
+        resize_dims=FLAGS.resolution,
+        batch_size=FLAGS.batch_size,
+        take=FLAGS.limit
+    )
+    val_dataset, val_size = cityscapes(
+        FLAGS.input,
+        state='val',
+        resize_dims=FLAGS.resolution,
+        batch_size=FLAGS.batch_size
+    )
+    fcn = resnet50_fcn(n_classes=num_classes)
+    adam = tf.keras.optimizers.Adam(lr=FLAGS.learning_rate)
+    b = 0
 
-# metrics
-avg_loss = tf.keras.metrics.Mean(name='loss', dtype=tf.float32)
-mIoU = tf.keras.metrics.MeanIoU(num_classes=num_classes, dtype=tf.float32)
+    # tb logs
+    train_summary_writer = tf.summary.create_file_writer('{}/{}/train'.format(FLAGS.log_dir, FLAGS.run))
+    val_summary_writer = tf.summary.create_file_writer('{}/{}/val'.format(FLAGS.log_dir, FLAGS.run))
+    if not os.path.exists(os.path.join('out', FLAGS.run)):
+        os.makedirs(os.path.join('out', FLAGS.run), exist_ok=True)
 
-for i in range(init_epoch, init_epoch + FLAGS.epoch):
-    with train_summary_writer.as_default():
-        print('train epoch ', i)
-        supervised_train_loop(fcn, adam, train_dataset, avg_loss, mIoU, iters=train_size // FLAGS.batch_size)
-        tf.summary.scalar('loss', avg_loss.result(), step=i)
-        tf.summary.scalar('mIoU', mIoU.result(), step=i)
-        avg_loss.reset_states()
-        mIoU.reset_states()
+    # checkpointing
+    ckpt, manager, init_epoch = checkpoints(adam, fcn)
+    # min_val_loss = np.inf
+    max_miou = 0
 
-    with val_summary_writer.as_default():
-        print('val epoch ', i)
-        val_loop(fcn, val_dataset, avg_loss, mIoU, iters=val_size // FLAGS.batch_size)
-        ckpt.step.assign_add(1)
+    # metrics
+    avg_loss = tf.keras.metrics.Mean(name='loss', dtype=tf.float32)
+    mIoU = tf.keras.metrics.MeanIoU(num_classes=num_classes, dtype=tf.float32)
 
-        tf.summary.scalar('loss', avg_loss.result(), step=i)
-        tf.summary.scalar('mIoU', mIoU.result(), step=i)
+    for i in range(init_epoch, init_epoch + FLAGS.epoch):
+        with train_summary_writer.as_default():
+            print('train epoch ', i)
+            supervised_train_loop(fcn, adam, train_dataset, avg_loss, mIoU, iters=train_size // FLAGS.batch_size)
+            tf.summary.scalar('loss', avg_loss.result(), step=i)
+            tf.summary.scalar('mIoU', mIoU.result(), step=i)
+            avg_loss.reset_states()
+            mIoU.reset_states()
 
-        if mIoU.result() > max_miou:
-            manager.save()
-            max_miou = mIoU.result()
+        with val_summary_writer.as_default():
+            print('val epoch ', i)
+            val_loop(fcn, val_dataset, avg_loss, mIoU, iters=val_size // FLAGS.batch_size)
+            ckpt.step.assign_add(1)
 
-        avg_loss.reset_states()
-        mIoU.reset_states()
+            tf.summary.scalar('loss', avg_loss.result(), step=i)
+            tf.summary.scalar('mIoU', mIoU.result(), step=i)
+
+            if mIoU.result() > max_miou:
+                manager.save()
+                max_miou = mIoU.result()
+
+            avg_loss.reset_states()
+            mIoU.reset_states()
+
+
+if __name__ == '__main__':
+    app.run(main)
